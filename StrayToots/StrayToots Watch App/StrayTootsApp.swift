8a97545e6eee6275aec1c054fd4d56133faed883
Jakayus <kawika@mail.usf.edu>
2023-04-15 14:23:05 -0700

    Add initial Watch and iPhone Project
    Created a WatchOS app project with companion iPhone app.

---
+++ StrayToots/StrayToots Watch App/StrayTootsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrayTootsApp.swift
+//  StrayToots Watch App
+//
+//  Created by Joel Sereno on 4/15/23.
+//
+
+import SwiftUI
+
+@main
+struct StrayToots_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StrayToots/StrayToots/StrayTootsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrayTootsApp.swift
+//  StrayToots
+//
+//  Created by Joel Sereno on 4/15/23.
+//
+
+import SwiftUI
+
+@main
+struct StrayTootsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
