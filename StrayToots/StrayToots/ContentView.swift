df5179e2e4c4938b11bfc11c2e32bf8ef2d4385d
Jakayus <kawika@mail.usf.edu>
2023-04-21 00:46:17 -0700

    Remove iPhone Toots and replace with settings TODO
    iPhone Toots removed as this project is focused specifically for the Watch. Instead, the iPhone app will focus on Settings and allowing the user to authenticate into their Mastodon instance.

--- StrayToots/StrayToots/ContentView.swift
+++ StrayToots/StrayToots/ContentView.swift
@@ -9,43 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @ObservedObject var MastodonAPI = MastodonNetworking()
-    
     var body: some View {
         VStack {
-            Text("Go time!")
-            Button("Refresh") {
-                MastodonAPI.fetchTimelineData()
-                print("test: \(MastodonAPI.toots.count)")
-            }
-            List {
-                ForEach(MastodonAPI.toots) {
-                    tempView(string1: $0.content, string2: "temp")
-                }
-            }
-            
+            Text("TODO: Settings portion of app")
+            Text("TODO: Allow user authentication here")
         }
-        
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(MastodonAPI: MastodonNetworking())
-    }
-}
-
-        
-// Temporary View that holds an individual toot
-struct tempView: View {
-    var string1 = ""
-    var string2 = ""
-    var body: some View {
-        VStack{
-            Text(string1)
-            Text(string2)
-            Label(/*@START_MENU_TOKEN@*/"Label"/*@END_MENU_TOKEN@*/, systemImage: /*@START_MENU_TOKEN@*/"42.circle"/*@END_MENU_TOKEN@*/)
-        }
+        ContentView()
     }
 }
